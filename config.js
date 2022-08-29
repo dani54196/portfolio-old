250f41f039cdc8ee4997584678f1c3227522ed49
Daniel Cardozo <dani54196@gmail.com>
2022-08-29 11:33:09 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteTitle: 'Gatsby portfolio secondary', // <title>
+  manifestName: 'Stylish',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/portfolio/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Daniel Cardozo',
+  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'icon-social-github',
+      name: 'Github',
+      url: 'https://github.com/dani54196',
+    },
+    {
+      icon: 'icon-social-instagram',
+      name: 'Twitter',
+      url: 'https://twitter.com/dani54196',
+    },
+    {
+      icon: 'icon-social-linkedin',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/dani54196',
+    },
+  ],
+  email: 'dani54196@gmail.com',
+  phone: '000-00000',
+  address: 'Buenos Aires, Argentina',
+};
